db934634ccb0b9b2d00da777f97e8b74e139d894
DESKTOP-221O1M5\Ludy <ludyaldy@gmail.com>
2021-09-29 09:17:12 +0700

    add unity project dan gitignore baru karena repository sebelumnya tidak sengaja terhapus

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    private static int highScore;
+    private int currentScore;
+
+    public int tileRatio;
+    public int comboRatio;
+
+    public int HighScore { get { return highScore; } }
+    public int CurrentScore { get { return currentScore; } }
+
+    #region Singleton
+    private static ScoreManager _instance = null;
+
+    public static ScoreManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<ScoreManager>();
+
+                if (_instance == null)
+                {
+                    Debug.LogError("Fatal Error: ScoreManager not Found");
+                }
+            }
+
+            return _instance;
+        }
+    }
+    #endregion
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetCurrentScore();
+    }
+
+    // Update is called once per frame
+    //void Update(){}
+
+    public void ResetCurrentScore()
+    {
+        currentScore = 0;
+    }
+
+    public void IncrementCurrentScore(int tileCount, int comboCount)
+    {
+        currentScore += (tileCount * tileRatio) * (comboCount * comboRatio);
+
+        SoundManager.Instance.PlayScore(comboCount > 1);
+        Debug.Log("current score : " + currentScore);
+    }
+
+    public void SetHighScore()
+    {
+        highScore = currentScore;
+        Debug.Log("highscore : " + highScore);
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Chapter 6 - Match Three
+
+Tugas Chapter 6 dari studi independen Agate Academy bidang game programming. Hal yang dibuat kali ini adalah game Match Three.
+
+### Hal yang sedikit diubah
+
+- Menambah waktu menjadi 60 detik
+
+
+
+Nama      : Abdurrahman Luqmanul Hakim
+
+ID Agate  : 135
